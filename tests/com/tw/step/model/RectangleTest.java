89fb5a3c47a0786487ceebb5ad9f9a775a49ea47
devdatta <183721457+devdattadhage@users.noreply.github.com>
2025-05-19 10:49:11 +0530

    Adds more test

--- tests/com/tw/step/model/RectangleTest.java
+++ tests/com/tw/step/model/RectangleTest.java
@@ -10,4 +10,16 @@ class RectangleTest {
         Rectangle rectangle = new Rectangle(2, 3);
         assertEquals(6, rectangle.area());
     }
+
+    @Test
+    void areaWithNoLength() {
+        Rectangle rectangle = new Rectangle(0, 3);
+        assertEquals(0, rectangle.area());
+    }
+
+    @Test
+    void areaWithNoBreath() {
+        Rectangle rectangle = new Rectangle(2, 0);
+        assertEquals(0, rectangle.area());
+    }
 }
